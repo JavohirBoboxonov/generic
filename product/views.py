c372abcfbe7f42496e580a323fa5b360ad3a44eb
JavohirBoboxonov <javohirboboxonov08@gmail.com>
2026-02-21 12:23:26 +0500

    big update

--- product/views.py
+++ product/views.py
@@ -6,12 +6,10 @@ from rest_framework import generics
 from .models import Book
 from .serializer import *
 
-# Ham ro'yxatni ko'rish, ham yangi kitob qo'shish uchun:
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = ProductSerializer
 
-# Bitta kitobni ko'rish, o'zgartirish yoki o'chirish uchun:
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = ProductSerializer
